54edb8f8b4a283e1f2aa01da9ce211547c8f3855
ashaik19 <anan.shaik@gmail.com>
2026-08-30 14:44:30 -0500

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.1
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "hola"
+print(type(variable))
+variable = [1]
+print(type(variable))
